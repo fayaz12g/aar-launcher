10b13127922cf10afcf61b6eda111ec59bd1c8b1
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-08-06 22:23:19 -0700

    always fetch latest

--- launcher.py
+++ launcher.py
@@ -26,27 +26,30 @@ username = getpass.getuser()
 aar_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\totk-aar'
 gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\totk-aar\\totk-aar-main'
 
+if os.path.exists(gui_dir):
+    shutil.rmtree(gui_dir)
+
 # Check if the directory exists
 if not os.path.exists(aar_dir):
     print(f"Directory '{aar_dir}' does not exist. Creating the directory...")
     os.makedirs(aar_dir)
 
-    # Download the contents of the GitHub repository
-    print("Downloading the contents of the GitHub repository...")
-    url = 'https://github.com/fayaz12g/totk-aar/archive/main.zip'
-    response = requests.get(url)
-    
-    # Save the downloaded content as a zip file
-    zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
-    with open(zip_file_path, 'wb') as zip_file:
-        zip_file.write(response.content)
-    
-    # Extract the zip file
-    with ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(aar_dir)
-    
-    # Remove the downloaded zip file
-    os.remove(zip_file_path)
+# Download the contents of the GitHub repository
+print("Downloading the contents of the GitHub repository...")
+url = 'https://github.com/fayaz12g/totk-aar/archive/main.zip'
+response = requests.get(url)
+
+# Save the downloaded content as a zip file
+zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
+with open(zip_file_path, 'wb') as zip_file:
+    zip_file.write(response.content)
+
+# Extract the zip file
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(aar_dir)
+
+# Remove the downloaded zip file
+os.remove(zip_file_path)
 
 # Add the directory to sys.path
 sys.path.append(gui_dir)
