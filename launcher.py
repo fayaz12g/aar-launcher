59f40a12255ddf7f1eb0edc88eb79cb981edc585
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-12-21 17:13:22 -0500

    updates

--- launcher.py
+++ launcher.py
@@ -22,15 +22,46 @@ import psutil
 import SarcLib
 import libyaz0
 
-def get_app_data_directory():
-    username = getpass.getuser()
-    aar_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio'
-    totk_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\totk-aar-main'
-    smo_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\smo-aar-main'
-    mk8d_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\mk8d-aar-main'
-    return aar_dir, totk_gui_dir, smo_gui_dir, mk8d_gui_dir
 
-def check_and_update_version(totk_gui_dir):
+#######################
+#### Create Window ####
+#######################
+
+tool_version = "2.0"
+
+username = getpass.getuser()
+aar_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio'
+    
+totk_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\totk-aar-main'
+smo_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\smo-aar-main'
+mk8d_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\mk8d-aar-main'
+
+
+# Create a Tkinter window to display the update progress
+def show_update_progress():
+    global aar_dir
+
+    # Check if the directory exists
+    if not os.path.exists(aar_dir):
+        print(f"Directory '{aar_dir}' does not exist. Creating the directory...")
+        os.makedirs(aar_dir)
+
+
+show_update_progress()
+
+root = customtkinter.CTk()
+root.title(f"Any Aspect Ratio Launcher {tool_version}")
+root.geometry("450x600")
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")  
+windowtitle = customtkinter.CTkLabel(master=root, font=(CTkFont, 20), text="Any Aspect Ratio Launcher {tool_version}")
+
+
+
+#TOTK Stuff
+
+def check_and_update_version_totk(totk_gui_dir):
     gui_path = os.path.join(totk_gui_dir, 'GUI.py')
     if os.path.exists(totk_gui_dir):
         with open(gui_path, 'r') as file:
@@ -54,13 +85,7 @@ def check_and_update_version(totk_gui_dir):
     else:
         return True
     
-#TOTK Stuff
-    
-#MK8D Stuff
-    
-#SMO Stuff
-
-def update_app_data(totk_gui_dir, aar_dir):
+def update_app_data_totk(totk_gui_dir, aar_dir):
     if os.path.exists(totk_gui_dir):
         shutil.rmtree(totk_gui_dir)
 
@@ -80,21 +105,106 @@ def update_app_data(totk_gui_dir, aar_dir):
 
     # Remove the downloaded zip file
     os.remove(zip_file_path)
+    
+#MK8D Stuff
+    
+def check_and_update_version_mk8d(totk_gui_dir):
+    gui_path = os.path.join(totk_gui_dir, 'GUI.py')
+    if os.path.exists(totk_gui_dir):
+        with open(gui_path, 'r') as file:
+            for line in file:
+                if line.startswith("tool_version"):
+                    current_version = line.split('=')[1].strip().strip('"')
+                    break
+        # Download the GUI.py from the main branch on GitHub
+        url = 'https://raw.githubusercontent.com/fayaz12g/mk8d-aar/main/GUI.py'
+        response = requests.get(url)
+        remote_version = None
+        for line in response.text.splitlines():
+            if line.startswith("tool_version"):
+                remote_version = line.split('=')[1].strip().strip('"')
+                break
+        if remote_version and current_version < remote_version:
+            print("New version available!")
+            return True
+        else:
+            return False
+    else:
+        return True
+    
+def update_app_data_mk8d(totk_gui_dir, aar_dir):
+    if os.path.exists(totk_gui_dir):
+        shutil.rmtree(totk_gui_dir)
 
-# Create a Tkinter window to display the update progress
-def show_update_progress():
+    # Download the contents of the GitHub repository
+    print("Downloading the contents of the GitHub repository...")
+    url = 'https://github.com/fayaz12g/mk8d-aar/archive/main.zip'
+    response = requests.get(url)
 
-    # Get the app data directory
-    aar_dir, totk_gui_dir, smo_gui_dir, mk8d_gui_dir = get_app_data_directory()
+    # Save the downloaded content as a zip file
+    zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
+    with open(zip_file_path, 'wb') as zip_file:
+        zip_file.write(response.content)
 
-    # Check if the directory exists
-    if not os.path.exists(aar_dir):
-        print(f"Directory '{aar_dir}' does not exist. Creating the directory...")
-        os.makedirs(aar_dir)
+    # Extract the zip file
+    with ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(aar_dir)
 
+    # Remove the downloaded zip file
+    os.remove(zip_file_path)
+
+#SMO Stuff
+    
+def check_and_update_version_smo(totk_gui_dir):
+    gui_path = os.path.join(totk_gui_dir, 'GUI.py')
+    if os.path.exists(totk_gui_dir):
+        with open(gui_path, 'r') as file:
+            for line in file:
+                if line.startswith("tool_version"):
+                    current_version = line.split('=')[1].strip().strip('"')
+                    break
+        # Download the GUI.py from the main branch on GitHub
+        url = 'https://raw.githubusercontent.com/fayaz12g/smo-aar/main/GUI.py'
+        response = requests.get(url)
+        remote_version = None
+        for line in response.text.splitlines():
+            if line.startswith("tool_version"):
+                remote_version = line.split('=')[1].strip().strip('"')
+                break
+        if remote_version and current_version < remote_version:
+            print("New version available!")
+            return True
+        else:
+            return False
+    else:
+        return True
+    
+def update_app_data_smo(totk_gui_dir, aar_dir):
+    if os.path.exists(totk_gui_dir):
+        shutil.rmtree(totk_gui_dir)
+
+    # Download the contents of the GitHub repository
+    print("Downloading the contents of the GitHub repository...")
+    url = 'https://github.com/fayaz12g/smo-aar/archive/main.zip'
+    response = requests.get(url)
+
+    # Save the downloaded content as a zip file
+    zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
+    with open(zip_file_path, 'wb') as zip_file:
+        zip_file.write(response.content)
+
+    # Extract the zip file
+    with ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(aar_dir)
+
+    # Remove the downloaded zip file
+    os.remove(zip_file_path)
+
+
+def launch_totk():
     # Check if an update is required
-    if check_and_update_version(totk_gui_dir):
-        update_app_data(totk_gui_dir, aar_dir)
+    if check_and_update_version_totk(totk_gui_dir):
+        update_app_data_totk(totk_gui_dir, aar_dir)
 
     # Add the directory to sys.path
     sys.path.append(totk_gui_dir)
@@ -102,5 +212,35 @@ def show_update_progress():
     # Now, you can import GUI
     import GUI
 
+def launch_mk8d():
+    if check_and_update_version_mk8d(mk8d_gui_dir):
+        update_app_data_mk8d(mk8d_gui_dir, aar_dir)
+
+    sys.path.append(mk8d_gui_dir)
+
+    # Now, you can import GUI
+    import GUI
+
+def launch_smo():
+    if check_and_update_version_smo(smo_gui_dir):
+        update_app_data_smo(smo_gui_dir, aar_dir)
+
+    sys.path.append(smo_gui_dir)
+
+    # Now, you can import GUI
+    import GUI
+
 # Automatically start the process
 show_update_progress()
+
+mario_button = customtkinter.CTkButton(master=root, text="AAR for Mario Odyssey", command=launch_smo)
+mario_button.pack(pady = 50)
+
+totk_button = customtkinter.CTkButton(master=root, text="AAR for Tears of the Kingdom", command=launch_totk)
+totk_button.pack(pady = 50)
+
+mk8d_button = customtkinter.CTkButton(master=root, text="AAR for Mariokart 8 Deluxe", command=launch_mk8d)
+mk8d_button.pack(pady = 50)
+
+
+root.mainloop()
