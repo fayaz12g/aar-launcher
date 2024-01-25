2af07daae80c70885bbf2e30ca9efe5916c8ee56
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-01-25 17:10:44 -0500

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -43,13 +43,14 @@ import zstandard
 #### Create Window ####
 #######################
 
-tool_version = "3.1"
+tool_version = "3.2"
 
 username = getpass.getuser()
 aar_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio'
     
 totk_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\totk-aar-main'
 smo_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\smo-aar-main'
+sm3dw_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\sm3dw-aar-main'
 mk8d_gui_dir = f'C:\\Users\\{username}\\AppData\\Roaming\\AnyAspectRatio\\mk8d-aar-main'
 
 dependencies = [
@@ -128,7 +129,7 @@ show_update_progress()
 
 root = customtkinter.CTk()
 root.title(f"Any Aspect Ratio Launcher {tool_version}")
-root.geometry("450x600")
+root.geometry("450x800")
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")  
@@ -187,10 +188,10 @@ def update_app_data_totk(totk_gui_dir, aar_dir):
     
 #MK8D Stuff
     
-def check_and_update_version_mk8d(totk_gui_dir):
+def check_and_update_version_mk8d(mk8d_gui_dir):
     # check_and_install_dependencies()
-    gui_path = os.path.join(totk_gui_dir, 'GUI.py')
-    if os.path.exists(totk_gui_dir):
+    gui_path = os.path.join(mk8d_gui_dir, 'GUI.py')
+    if os.path.exists(mk8d_gui_dir):
         with open(gui_path, 'r') as file:
             for line in file:
                 if line.startswith("tool_version"):
@@ -212,9 +213,9 @@ def check_and_update_version_mk8d(totk_gui_dir):
     else:
         return True
     
-def update_app_data_mk8d(totk_gui_dir, aar_dir):
-    if os.path.exists(totk_gui_dir):
-        shutil.rmtree(totk_gui_dir)
+def update_app_data_mk8d(mk8d_gui_dir, aar_dir):
+    if os.path.exists(mk8d_gui_dir):
+        shutil.rmtree(mk8d_gui_dir)
 
     # Download the contents of the GitHub repository
     print("Downloading the contents of the GitHub repository...")
@@ -222,7 +223,7 @@ def update_app_data_mk8d(totk_gui_dir, aar_dir):
     response = requests.get(url)
 
     # Save the downloaded content as a zip file
-    zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
+    zip_file_path = os.path.join(aar_dir, 'mk8d-aar-main.zip')
     with open(zip_file_path, 'wb') as zip_file:
         zip_file.write(response.content)
 
@@ -235,10 +236,10 @@ def update_app_data_mk8d(totk_gui_dir, aar_dir):
 
 #SMO Stuff
     
-def check_and_update_version_smo(totk_gui_dir):
+def check_and_update_version_smo(smo_gui_dir):
     # check_and_install_dependencies()
-    gui_path = os.path.join(totk_gui_dir, 'GUI.py')
-    if os.path.exists(totk_gui_dir):
+    gui_path = os.path.join(smo_gui_dir, 'GUI.py')
+    if os.path.exists(smo_gui_dir):
         with open(gui_path, 'r') as file:
             for line in file:
                 if line.startswith("tool_version"):
@@ -260,9 +261,9 @@ def check_and_update_version_smo(totk_gui_dir):
     else:
         return True
     
-def update_app_data_smo(totk_gui_dir, aar_dir):
-    if os.path.exists(totk_gui_dir):
-        shutil.rmtree(totk_gui_dir)
+def update_app_data_smo(smo_gui_dir, aar_dir):
+    if os.path.exists(smo_gui_dir):
+        shutil.rmtree(smo_gui_dir)
 
     # Download the contents of the GitHub repository
     update_text("Downloading the contents of the GitHub repository...")
@@ -270,7 +271,7 @@ def update_app_data_smo(totk_gui_dir, aar_dir):
     response = requests.get(url)
 
     # Save the downloaded content as a zip file
-    zip_file_path = os.path.join(aar_dir, 'totk-aar-main.zip')
+    zip_file_path = os.path.join(aar_dir, 'smo-aar-main.zip')
     with open(zip_file_path, 'wb') as zip_file:
         zip_file.write(response.content)
 
@@ -280,6 +281,55 @@ def update_app_data_smo(totk_gui_dir, aar_dir):
 
     # Remove the downloaded zip file
     os.remove(zip_file_path)
+
+#SM3DW+BF Stuff
+
+def check_and_update_version_sm3dw(sm3dw_gui_dir):
+    # check_and_install_dependencies()
+    gui_path = os.path.join(sm3dw_gui_dir, 'GUI.py')
+    if os.path.exists(sm3dw_gui_dir):
+        with open(gui_path, 'r') as file:
+            for line in file:
+                if line.startswith("tool_version"):
+                    current_version = line.split('=')[1].strip().strip('"')
+                    break
+        # Download the GUI.py from the main branch on GitHub
+        url = 'https://raw.githubusercontent.com/fayaz12g/sm3dw-aar/main/GUI.py'
+        response = requests.get(url)
+        remote_version = None
+        for line in response.text.splitlines():
+            if line.startswith("tool_version"):
+                remote_version = line.split('=')[1].strip().strip('"')
+                break
+        if remote_version and current_version < remote_version:
+            update_text("New version available!")
+            return True
+        else:
+            return False
+    else:
+        return True
+    
+def update_app_data_sm3dw(sm3dw_gui_dir, aar_dir):
+    if os.path.exists(sm3dw_gui_dir):
+        shutil.rmtree(sm3dw_gui_dir)
+
+    # Download the contents of the GitHub repository
+    update_text("Downloading the contents of the GitHub repository...")
+    url = 'https://github.com/fayaz12g/sm3dw-aar/archive/main.zip'
+    response = requests.get(url)
+
+    # Save the downloaded content as a zip file
+    zip_file_path = os.path.join(aar_dir, 'sm3dw-aar-main.zip')
+    with open(zip_file_path, 'wb') as zip_file:
+        zip_file.write(response.content)
+
+    # Extract the zip file
+    with ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(aar_dir)
+
+    # Remove the downloaded zip file
+    os.remove(zip_file_path)
+
 
 
 def launch_totk():
@@ -358,6 +408,28 @@ def launch_smo():
     except subprocess.CalledProcessError as e:
         update_text(f"Error: {e}")
 
+def launch_sm3dw():
+    if check_and_update_version_sm3dw(sm3dw_gui_dir):
+        update_app_data_sm3dw(sm3dw_gui_dir, aar_dir)
+
+    root.destroy()
+    # Specify the path to the Python script you want to launch
+    sm3dw_gui = os.path.join(sm3dw_gui_dir, 'GUI.py')
+    # Get the path to the current executable (the PyInstaller-built application)
+    current_executable = sys.executable
+
+    # Build the command to execute the other Python script
+    launch_sm3dw_command = ["python", sm3dw_gui]
+   
+    # Launch Using Old Method
+    sys.path.append(sm3dw_gui_dir)
+
+    # Use subprocess to launch the script
+    try:
+        import GUI
+        # subprocess.run(launch_sm3dw_command, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+    except subprocess.CalledProcessError as e:
+        update_text(f"Error: {e}")
 
 def update_text(new_text):
     text_box.config(state="normal")  # Set state to normal to enable editing
@@ -377,6 +449,9 @@ totk_button.pack(pady = 50)
 
 mk8d_button = customtkinter.CTkButton(master=root, text="AAR for MarioKart 8 Deluxe", command=launch_mk8d)
 mk8d_button.pack(pady = 50)
+
+sm3dw_button = customtkinter.CTkButton(master=root, text="AAR for Super Mario 3D World + Bowser's Fury", command=launch_sm3dw)
+sm3dw_button.pack(pady = 50)
 
 text_box = scrolledtext.ScrolledText(master=root, wrap="word", height=30, width=70)
 text_box.pack(pady=20)
