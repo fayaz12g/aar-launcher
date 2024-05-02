58f09b0761f199b6186b3d56b4be98afe0264a34
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-02 15:56:03 -0400

    new ui

--- local.py
+++ local.py
@@ -37,40 +37,8 @@ import urllib3
 from keystone import *
 import pyautogui
 import zstandard
-
-
-
-
-#######################
-#### Create Window ####
-#######################
-
-tool_version = "2.3"
-
-username = getpass.getuser()
-aar_dir = f'C:\\Users\\{username}\\Documents\\GitHub'
-
-aar_tools_short = ['totk', 'smo', 'sm3dw', 'mk8d', 'mm2', 'ssbu', 'mvdk', '12switch']
-aar_names = ['Tears of the Kingdom', 'Super Mario Odyssey', 'Super Mario 3D World', 'MarioKart 8 Deluxe', 'Super Mario Maker 2', 'Super Smash Brothers Ultimate', 'Mario vs. Donkey Kong', '1-2 Switch']
-
-aar_tools = [
-    {'1': 'totk', '2': 'Tears of the Kingdom'},
-    {'1': 'smo', '2': 'Super Mario Odyssey'},
-    {'1': 'sm3dw', '2': 'Super Mario 3D World'},
-    {'1': 'mk8d', '2': 'MarioKart 8 Deluxe'},
-    {'1': 'mm2', '2': 'Super Mario Maker 2'},
-    {'1': 'ssbu', '2': 'Super Smash Brothers Ultimate'},
-    {'1': 'mvdk', '2': 'Mario vs. Donkey Kong'},
-    {'1': '12switch', '2': '1-2 Switch'},
-    {'1': 'splatoon3', '2': 'Splatoon 3'},
-    {'1': 'splatoon2', '2': 'Splatoon 2'},
-    {'1': 'pmtok', '2': 'Paper Mario: The Origami King'}
-]
-
-gui_dirs = {}
-
-for tool in aar_tools:
-    gui_dirs[tool['1']] = f'C:\\Users\\{username}\\Documents\\GitHub\\{tool["1"]}-aar'
+import tkinter as tk
+from tkinter import ttk
 
 dependencies = [
     "packaging",
@@ -91,11 +59,9 @@ dependencies = [
     "pyautogui",
 ]
 
-def update_text(new_text):
-    text_box.config(state="normal")  # Set state to normal to enable editing
-    text_box.delete(1.0, "end")  # Clear existing text
-    text_box.insert("end", new_text)  # Insert new text
-    text_box.config(state="disabled")  # Set state to disabled to make it read-only
+#######################
+## Helper Functions ###
+#######################
     
 def is_pip_installed():
     try:
@@ -108,32 +74,60 @@ def is_pip_installed():
 def install_pip():
     try:
         subprocess.run(["python", "-m", "ensurepip", "--default-pip"], check=True)
-        update_text("pip has been successfully installed.")
+        print("pip has been successfully installed.")
     except subprocess.CalledProcessError:
-        update_text("Failed to install pip. Please install it manually.")
+        print("Failed to install pip. Please install it manually.")
 
 
 # Function to check and install dependencies
 def check_and_install_dependencies():
     if not is_pip_installed():
-        update_text("pip is not installed. Attempting to install pip...")
+        print("pip is not installed. Attempting to install pip...")
         install_pip()
         
     for dependency in dependencies:
         try:
             __import__(dependency)
-            update_text(f"{dependency} is already installed.")
+            print(f"{dependency} is already installed.")
         except ImportError:
-            update_text(f"{dependency} is not installed. Attempting to install...")
+            print(f"{dependency} is not installed. Attempting to install...")
             install_dependency(dependency)
 
 # Function to install a specific dependency using pip
 def install_dependency(dependency):
     try:
         subprocess.run(["python", "-m", "pip", "install", dependency], check=True)
-        update_text(f"{dependency} has been successfully installed.")
+        print(f"{dependency} has been successfully installed.")
     except subprocess.CalledProcessError:
-        update_text(f"Failed to install {dependency}. Please install it manually.")
+        print(f"Failed to install {dependency}. Please install it manually.")
+
+#######################
+#### Create Window ####
+#######################
+
+tool_version = "3.0"
+
+username = getpass.getuser()
+aar_dir = f'C:\\Users\\{username}\\Documents\\GitHub'
+
+aar_tools = [
+    {'1': 'totk', '2': 'Tears of the Kingdom'},
+    {'1': 'smo', '2': 'Super Mario Odyssey'},
+    {'1': 'sm3dw', '2': 'Super Mario 3D World'},
+    {'1': 'mk8d', '2': 'MarioKart 8 Deluxe'},
+    {'1': 'mm2', '2': 'Super Mario Maker 2'},
+    {'1': 'ssbu', '2': 'Super Smash Brothers Ultimate'},
+    {'1': 'mvdk', '2': 'Mario vs. Donkey Kong'},
+    {'1': '12switch', '2': '1-2 Switch'},
+    {'1': 'splatoon3', '2': 'Splatoon 3'},
+    {'1': 'splatoon2', '2': 'Splatoon 2'},
+    {'1': 'pmtok', '2': 'Paper Mario: The Origami King'}
+]
+
+gui_dirs = {}
+
+for tool in aar_tools:
+    gui_dirs[tool['1']] = f'C:\\Users\\{username}\\Documents\\GitHub\\{tool["1"]}-aar'
 
 # Create a Tkinter window to display the update progress
 def show_update_progress():
@@ -141,62 +135,67 @@ def show_update_progress():
 
     # Check if the directory exists
     if not os.path.exists(aar_dir):
-        update_text(f"Directory '{aar_dir}' does not exist. Creating the directory...")
+        print(f"Directory '{aar_dir}' does not exist. Creating the directory...")
         os.makedirs(aar_dir)
-
-
-show_update_progress()
 
 root = customtkinter.CTk()
 root.title(f"Any Aspect Ratio Launcher {tool_version}")
-root.geometry("450x800")
+root.geometry("450x300")
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")  
 windowtitle = customtkinter.CTkLabel(master=root, font=(CTkFont, 20), text="Any Aspect Ratio Launcher {tool_version}")
 
+titlebar = customtkinter.CTkLabel(text="Select a Game, then Click the Launch AAR Button:", master = root)
+titlebar.pack(pady=50)
 
-def launch_tool(tool_name):
-    root.destroy()  # Assuming `root` is defined elsewhere in your code
-    # Specify the path to the Python script you want to launch
-    gui_script = os.path.join(gui_dirs[tool_name], 'GUI.py')
+# Update the button's state when a tool is selected
+def update_button_state(choice):
+    if choice != "Select a Game":
+        launch_button.configure(state=tkinter.NORMAL)
+    else:
+        launch_button.configure(state=tkinter.DISABLED)
 
-    # Get the path to the current executable (the PyInstaller-built application)
-    current_executable = sys.executable
+# Create a dropdown menu
+combo = customtkinter.CTkComboBox(root, state="readonly", values = [tool["2"] for tool in aar_tools], width = 250, hover=True, command = update_button_state)
+combo['values'] = [tool["2"] for tool in aar_tools]
+combo.pack(pady=20)
 
-    # Build the command to execute the other Python script
-    launch_command = [current_executable, gui_script]
+combo.set("Select a Game")
 
-    # Launch Using Old Method
-    sys.path.append(gui_dirs[tool_name])
+# Create a dictionary that maps full tool names to short tool names
+tool_name_map = {tool["2"]: tool["1"] for tool in aar_tools}
 
-    # Use subprocess to launch the script
-    try:
-        import GUI
-        # subprocess.run(launch_command, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
-    except subprocess.CalledProcessError as e:
-        update_text(f"Error: {e}")
+# Create a button to launch the selected tool
+launch_button = customtkinter.CTkButton(root, text="Launch AAR", state=tkinter.DISABLED, hover=True, text_color="white")
+launch_button.pack(pady=20)
 
+def launch_tool(event):
+    full_tool_name = combo.get()
+    if full_tool_name != "Select a Game":
+        tool_name = tool_name_map[full_tool_name]
+        root.destroy() 
+        gui_script = os.path.join(gui_dirs[tool_name], 'GUI.py')
 
+        # Get the path to the current executable (the PyInstaller-built application)
+        current_executable = sys.executable
 
-def update_text(new_text):
-    text_box.config(state="normal")  # Set state to normal to enable editing
-    text_box.delete(1.0, "end")  # Clear existing text
-    text_box.insert("end", new_text)  # Insert new text
-    text_box.config(state="disabled")  # Set state to disabled to make it read-only
+        # Build the command to execute the other Python script
+        launch_command = [current_executable, gui_script]
 
+        # Launch Using Old Method
+        sys.path.append(gui_dirs[tool_name])
+
+        # Use subprocess to launch the script
+        try:
+            import GUI
+            # subprocess.run(launch_command, check=True, creationflags=subprocess.CREATE_NO_WINDOW)
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+
+launch_button.bind("<Button-1>", launch_tool)
 
 # Automatically start the process
 show_update_progress()
-
-for tool in aar_tools:
-    button = customtkinter.CTkButton(master=root, text=f"AAR for {tool["2"]}", command=lambda t=tool["1"]: launch_tool(t))
-    button.pack(pady=20)
-
-text_box = scrolledtext.ScrolledText(master=root, wrap="word", height=30, width=70)
-text_box.pack(pady=20)
-
-default_text = "Click the game you want to use the tool for!"
-update_text(default_text)
 
 root.mainloop()
