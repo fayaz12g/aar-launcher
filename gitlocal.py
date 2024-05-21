e52d8c881ff3391e11c3a3b1ee346a2b6251b180
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-21 01:37:59 -0400

    update

--- gitlocal.py
+++ gitlocal.py
@@ -105,7 +105,7 @@ def install_dependency(dependency):
 #### Create Window ####
 #######################
 
-tool_version = "6.1"
+tool_version = "6.3"
 
 username = getpass.getuser()
 aar_dir = f'C:\\Users\\{username}\\Documents\\GitHub'
--- launcher.py
+++ launcher.py
@@ -115,7 +115,7 @@ def show_update_progress():
 #### Create Window ####
 #######################
 
-tool_version = "6.2"
+tool_version = "6.3"
 
 # Determine the user's home directory based on the platform
 if sys.platform == 'win32':
